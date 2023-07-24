b68fcb7b28c8e627af25ca511d19a0b43318c65b
877wrangler <877wrangler@gmail.com>
2023-07-24 17:45:57 -0400

    Change about picture

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ import ProjectsPage from "../components/Projects";
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-2 bg-slate-100'>
-            <h1 className='fixed invisible md:visible'>GLF Siding</h1>
+            <h1 className='fixed invisible md:visible' style={{marginTop: "-10px"}}>GLF Siding</h1>
             <h2 className='fixed md:invisible'>GLF Siding</h2>
             <Navbar />
             <Main />
