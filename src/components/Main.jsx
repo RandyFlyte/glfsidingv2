6527ace3adf9bb1266d6a15e28b10b931f69332b
877wrangler <877wrangler@gmail.com>
2023-07-24 18:03:18 -0400

    Add background image to main, change text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import AboutImg from "../../public/assets/about.jpg";
 
 const Main = () => {
     return (
-        <div className='w-full h-screen text-center'>
-            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-                <div>
-                    <p className='uppercase text-sm tracking-widest text-gray-600'>
-                        We at{" "}
-                        <span className='py-4 text-gray=700'>
-                            GLF Siding Specialize in Siding, Windows, and Doors!
+        <div className='w-full h-screen text-center relative mt-48'>
+            <Image src={AboutImg} layout='fill' objectFit='cover' className='rounded-xl' alt='/' />
+
+            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center absolute top-0'>
+                <div className="text-gray-200">
+                    <p className='uppercase text-sm tracking-widest text-gray-200 bg-slate-400'>
+                        We at GLF Siding Specialize in Siding, Windows, and Doors!
+                        <span className='py-4 bg-slate-400'>
+                            {/* GLF Siding Specialize in Siding, Windows, and Doors! */}
                         </span>
                     </p>
-                    <h1>Get a free estimate now!</h1>
+                    <h1 className="text-gray-100 bg-slate-500">Get a free estimate now!</h1>
                 </div>
             </div>
         </div>
