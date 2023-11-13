3313d07ce3d57e09b44e039bfc27550e7179ce39
Randy Flyte <877wrangler@gmail.com>
2023-11-12 19:56:09 -0500

    Fix email

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,11 +18,11 @@ const About = () => {
             commitment to customer satisfaction. We invite you to reach out with
             any inquiries or to request a complimentary estimate.
           </p>
-          <Link href='/#projects'>
+          {/* <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
             </p>
-          </Link>
+          </Link> */}
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
           {/* <Image src={AboutImg} className='rounded-xl' alt='/' /> */}
